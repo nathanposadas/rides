b01049117ad45a6e523a896f8bfbafaee11d4d3a
Jonathan Posadas <148729777+nathanposadas@users.noreply.github.com>
2024-09-27 00:51:28 +0800

    v1.2

---
+++ app/(auth)/option.tsx
@@ -0,0 +1,13 @@
+//ito yung sa may choose option kung passenger ba o driver, pagtapos ng swiper
+
+import { Text} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const option =() => {
+  return(
+  <SafeAreaView>
+    <Text>SanToda</Text>
+  </SafeAreaView>
+  )
+}
+export default option;
